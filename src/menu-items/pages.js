7beadd2fac8415370e7eb4756db24dfb23302607
Induwara-Nagodavithana <induwara.nagodavithana@gmail.com>
2022-02-23 00:02:42 +0530

    changed

--- src/menu-items/pages.js
+++ src/menu-items/pages.js
@@ -1,5 +1,18 @@
 // assets
-import { IconKey, IconReceipt2, IconBug, IconBellRinging, IconPhoneCall } from '@tabler/icons';
+import {
+    IconKey,
+    IconReceipt2,
+    IconBug,
+    IconBellRinging,
+    IconPhoneCall,
+    IconCash,
+    IconBuildingCommunity,
+    IconBrandCodesandbox,
+    IconBuildingArch,
+    IconUsers,
+    IconReportAnalytics,
+    IconReceipt
+} from '@tabler/icons';
 
 // constant
 const icons = {
@@ -7,7 +20,8 @@ const icons = {
     IconReceipt2,
     IconBug,
     IconBellRinging,
-    IconPhoneCall
+    IconPhoneCall,
+    IconCash
 };
 
 // ===========================|| EXTRA PAGES MENU ITEMS ||=========================== //
@@ -18,34 +32,82 @@ const pages = {
     caption: 'Pages Caption',
     type: 'group',
     children: [
-        {
-            id: 'authentication',
-            title: 'Authentication',
-            type: 'collapse',
-            icon: icons.IconKey,
-            children: [
-                {
-                    id: 'login3',
-                    title: 'Login',
-                    type: 'item',
-                    url: '/pages/login/login3',
-                    target: true
-                },
-                {
-                    id: 'register3',
-                    title: 'Register',
-                    type: 'item',
-                    url: '/pages/register/register3',
-                    target: true
-                }
-            ]
-        },
+        // {
+        //     id: 'authentication',
+        //     title: 'Authentication',
+        //     type: 'collapse',
+        //     icon: icons.IconKey,
+        //     children: [
+        //         {
+        //             id: 'login3',
+        //             title: 'Login',
+        //             type: 'item',
+        //             url: '/pages/login/login3',
+        //             target: true
+        //         },
+        //         {
+        //             id: 'register3',
+        //             title: 'Register',
+        //             type: 'item',
+        //             url: '/pages/register/register3',
+        //             target: true
+        //         }
+        //     ]
+        // },
         {
             id: 'customerPayment',
             title: 'Customer Payment',
             type: 'item',
             url: '/pages/customerPayment',
-            icon: icons.IconDashboard,
+            icon: icons.IconCash,
+            breadcrumbs: false
+        },
+        {
+            id: 'subscriptionPayment',
+            title: 'Subscription',
+            type: 'item',
+            url: '/pages/subscription',
+            icon: IconReceipt,
+            breadcrumbs: false
+        },
+        {
+            id: 'customerPayment1',
+            title: 'Subscription Type',
+            type: 'item',
+            url: '/sample-page',
+            icon: IconBrandCodesandbox,
+            breadcrumbs: false
+        },
+        {
+            id: 'customerPayment2',
+            title: 'Gym Managment',
+            type: 'item',
+            url: '/sample-page',
+            icon: IconBuildingCommunity,
+            breadcrumbs: false
+        },
+        {
+            id: 'customerPayment3',
+            title: 'Branch Managment',
+            type: 'item',
+            url: '/sample-page',
+            icon: IconBuildingArch,
+            breadcrumbs: false
+        },
+        {
+            id: 'customerPayment4',
+            title: 'Services',
+            type: 'item',
+            url: '/sample-page',
+            icon: IconReportAnalytics,
+            breadcrumbs: false
+        },
+        {
+            id: 'customerPayment5',
+            title: 'Member Managment',
+            type: 'item',
+            url: '/sample-page',
+            icon: IconUsers,
             breadcrumbs: false
         }
     ]
